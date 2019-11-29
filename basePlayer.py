b4f1e7168961cf63a1704450f4ee11e127c66a7b
Gbor97 <37269508+Gbor97@users.noreply.github.com>
2019-11-29 10:07:21 +0100

    Add files via upload

--- basePlayer.py
+++ basePlayer.py
@@ -1,12 +1,20 @@
 from generals import PlayerEnum
+from generals import State
 from enum import Enum
 
 class BasePlayer:
-    def __init__(self, sign, type, board, cont):
-        self.sign = sign
-        self.type = PlayerEnum(type)
+    def __init__(self, board):
+        self.sign = State.EMPTY
+        self.rival_sign = State.EMPTY
         self.board = board
-        self.cont = cont
 
     def move(self, moveX, moveY):
         raise NotImplementedError()
+
+    def setSign(self, Start):
+        if Start:
+            self.sign = State.X
+            self.rival_sign = State.O
+        else:
+            self.sign = State.O
+            self.rival_sign = State.X
